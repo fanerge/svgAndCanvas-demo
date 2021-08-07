6cacd9777e55af5a85437f4a970e047719716157
fanerge <fanerge@qq.com>
2021-08-07 23:15:25 +0800

    feat: canvas time

--- .history/src/pages/home_20210807231435.vue
+++ .history/src/pages/home_20210807231435.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <div>系统学习 SVG 和 Canvas 案列</div>
+    <div>系统学习 SVG 和 Canvas</div>
     <router-link to="/CanvasTime">Canvas实现数字时钟 </router-link>
     <router-link to="/SvgLight">SvgLight</router-link>
   </div>
