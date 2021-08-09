299ddf904a305654f193728a7efd13da7c84ae4f
fanerge <fanerge@qq.com>
2021-08-09 21:30:05 +0800

    feat: test

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -1,10 +1,18 @@
 <template>
   <div>
     <div>系统学习 SVG 和 Canvas 案列</div>
-    <router-link tag="p" to="/CanvasTime">Canvas 实现数字时钟</router-link>
-    <router-link tag="p" to="/SvgLight">Svg 实现海岸和星空</router-link>
-    <router-link tag="p" to="/SvgSpring">Svg 实现弹簧自平衡物理模型</router-link>
+    <router-link class="menu-item" to="/CanvasTime">Canvas 实现数字时钟</router-link>
+    <router-link class="menu-item" to="/SvgLight">Svg 实现海岸和星空</router-link>
+    <router-link class="menu-item" to="/SvgSpring">Svg 实现弹簧自平衡物理模型</router-link>
+    <router-link class="menu-item" to="/SvgWiget">Svg 小组件</router-link>
+
   </div>
 </template>
-<script setup>
+<script>
 </script>
+<style scoped>
+.menu-item {
+  margin-top: 6px;
+  display: block;
+}
+</style>
